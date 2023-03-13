c7c8bf8a7916b891c01c0dc2d5257ca7da7d836e
nadabazzal <127792861+nadabazzal@users.noreply.github.com>
2023-03-13 22:30:10 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+function bigImg(x) {
+    x.style.color = 'white';
+    
+  }
+  
+  function normalImg(x) {
+    x.style.color =' red';
+   
+  }
+  
+  function big(x) {
+    x.style.color = 'red';
+    
+  }
+  
+  function normal(x) {
+    x.style.color =' white';
+   
+  }
+  
